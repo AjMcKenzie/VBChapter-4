72a19c1358a6b9a3727bd0a676901c2c627d55d6
Andrew <heldiggris7@gmail.com>
2017-11-16 11:32:04 -0600

    update StringFinder

---
+++ StringFinder/StringFinder/Form1.Designer.vb
@@ -0,0 +1,231 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lblString = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtToFind = New System.Windows.Forms.TextBox()
+        Me.txtStartIndex = New System.Windows.Forms.TextBox()
+        Me.btnClose = New System.Windows.Forms.Button()
+        Me.lblResults = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.btnGo = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(2, 17)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(93, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Search this String:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(14, 50)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(81, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Index Numbers:"
+        '
+        'lblString
+        '
+        Me.lblString.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblString.Font = New System.Drawing.Font("Courier New", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblString.ForeColor = System.Drawing.SystemColors.HotTrack
+        Me.lblString.Location = New System.Drawing.Point(115, 17)
+        Me.lblString.Name = "lblString"
+        Me.lblString.Size = New System.Drawing.Size(385, 20)
+        Me.lblString.TabIndex = 3
+        Me.lblString.Text = "abcABC00123573@ 123 ABC 98ABC123"
+        Me.lblString.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.Label4.ImageAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.Label4.Location = New System.Drawing.Point(60, 17)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(86, 13)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "Substring to find:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.Label5.ImageAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.Label5.Location = New System.Drawing.Point(25, 47)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(121, 13)
+        Me.Label5.TabIndex = 5
+        Me.Label5.Text = "Start searching at index:"
+        '
+        'txtToFind
+        '
+        Me.txtToFind.Location = New System.Drawing.Point(153, 17)
+        Me.txtToFind.Name = "txtToFind"
+        Me.txtToFind.Size = New System.Drawing.Size(100, 20)
+        Me.txtToFind.TabIndex = 6
+        '
+        'txtStartIndex
+        '
+        Me.txtStartIndex.Location = New System.Drawing.Point(153, 47)
+        Me.txtStartIndex.Name = "txtStartIndex"
+        Me.txtStartIndex.Size = New System.Drawing.Size(47, 20)
+        Me.txtStartIndex.TabIndex = 7
+        '
+        'btnClose
+        '
+        Me.btnClose.Location = New System.Drawing.Point(231, 239)
+        Me.btnClose.Name = "btnClose"
+        Me.btnClose.Size = New System.Drawing.Size(75, 23)
+        Me.btnClose.TabIndex = 9
+        Me.btnClose.Text = "Close"
+        Me.btnClose.UseVisualStyleBackColor = True
+        '
+        'lblResults
+        '
+        Me.lblResults.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.lblResults.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblResults.Location = New System.Drawing.Point(53, 90)
+        Me.lblResults.Name = "lblResults"
+        Me.lblResults.Size = New System.Drawing.Size(258, 23)
+        Me.lblResults.TabIndex = 10
+        Me.lblResults.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'Label6
+        '
+        Me.Label6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Label6.Font = New System.Drawing.Font("Courier New", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Label6.Location = New System.Drawing.Point(115, 50)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(385, 20)
+        Me.Label6.TabIndex = 3
+        Me.Label6.Text = "012345678901234567890123456789012"
+        Me.Label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(239, 81)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(21, 13)
+        Me.Label7.TabIndex = 11
+        Me.Label7.Text = "10"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(336, 81)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(21, 13)
+        Me.Label8.TabIndex = 11
+        Me.Label8.Text = "20"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label9.Location = New System.Drawing.Point(437, 81)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(21, 13)
+        Me.Label9.TabIndex = 11
+        Me.Label9.Text = "30"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.btnGo)
+        Me.GroupBox1.Controls.Add(Me.lblResults)
+        Me.GroupBox1.Controls.Add(Me.txtStartIndex)
+        Me.GroupBox1.Controls.Add(Me.txtToFind)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Location = New System.Drawing.Point(78, 97)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(390, 136)
+        Me.GroupBox1.TabIndex = 12
+        Me.GroupBox1.TabStop = False
+        '
+        'btnGo
+        '
+        Me.btnGo.Location = New System.Drawing.Point(220, 45)
+        Me.btnGo.Name = "btnGo"
+        Me.btnGo.Size = New System.Drawing.Size(47, 23)
+        Me.btnGo.TabIndex = 11
+        Me.btnGo.Text = "Go"
+        Me.btnGo.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(516, 271)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.btnClose)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.lblString)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "String Finder"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents lblString As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents txtToFind As System.Windows.Forms.TextBox
+    Friend WithEvents txtStartIndex As System.Windows.Forms.TextBox
+    Friend WithEvents btnClose As System.Windows.Forms.Button
+    Friend WithEvents lblResults As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents btnGo As System.Windows.Forms.Button
+
+End Class
---
+++ StringFinder/StringFinder/Form1.vb
@@ -0,0 +1,25 @@
+﻿Public Class Form1
+    Private Sub btnGo_Click_1(sender As Object, e As EventArgs) Handles btnGo.Click
+        Dim intStartIndex, intFoundIndex As Integer
+
+        If IsNumeric(txtStartIndex.Text) Then
+            If txtToFind.Text.Length > 0 Then
+                intStartIndex = CInt(txtStartIndex.Text)
+                intFoundIndex = lblString.Text.IndexOf(txtToFind.Text, intStartIndex)
+                If intFoundIndex = -1 Then
+                    lblResults.Text = "The string was not found."
+                Else
+                    lblResults.Text = "The string was found at index " & intFoundIndex
+                End If
+            Else
+                MessageBox.Show("Enter a string to search.")
+            End If
+        Else
+            MessageBox.Show("The index must be a number.")
+        End If
+    End Sub
+
+    Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+        Me.Close()
+    End Sub
+End Class
