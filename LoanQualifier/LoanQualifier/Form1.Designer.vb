072cb6703187d4f4f39c626f407971a99f44e9b1
Andrew <heldiggris7@gmail.com>
2017-11-16 10:04:42 -0600

    update LoanQualifier

---
+++ LoanQualifier/LoanQualifier/Form1.Designer.vb
@@ -0,0 +1,145 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lblMessage = New System.Windows.Forms.Label()
+        Me.txtAnnualSalary = New System.Windows.Forms.TextBox()
+        Me.txtYearsAtJob = New System.Windows.Forms.TextBox()
+        Me.btnCheck = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.txtYearsAtJob)
+        Me.GroupBox1.Controls.Add(Me.txtAnnualSalary)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(28, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(382, 123)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Enter the following Information:"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(31, 35)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(75, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Annual Salary:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(31, 70)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(96, 26)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Number of years at" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "your current job:"
+        '
+        'lblMessage
+        '
+        Me.lblMessage.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblMessage.Location = New System.Drawing.Point(28, 151)
+        Me.lblMessage.Name = "lblMessage"
+        Me.lblMessage.Size = New System.Drawing.Size(396, 18)
+        Me.lblMessage.TabIndex = 2
+        Me.lblMessage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'txtAnnualSalary
+        '
+        Me.txtAnnualSalary.Location = New System.Drawing.Point(166, 35)
+        Me.txtAnnualSalary.Name = "txtAnnualSalary"
+        Me.txtAnnualSalary.Size = New System.Drawing.Size(65, 20)
+        Me.txtAnnualSalary.TabIndex = 2
+        '
+        'txtYearsAtJob
+        '
+        Me.txtYearsAtJob.Location = New System.Drawing.Point(166, 76)
+        Me.txtYearsAtJob.Name = "txtYearsAtJob"
+        Me.txtYearsAtJob.Size = New System.Drawing.Size(65, 20)
+        Me.txtYearsAtJob.TabIndex = 3
+        '
+        'btnCheck
+        '
+        Me.btnCheck.Location = New System.Drawing.Point(62, 192)
+        Me.btnCheck.Name = "btnCheck"
+        Me.btnCheck.Size = New System.Drawing.Size(85, 34)
+        Me.btnCheck.TabIndex = 3
+        Me.btnCheck.Text = "Check Qualifications"
+        Me.btnCheck.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(184, 192)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(85, 34)
+        Me.btnClear.TabIndex = 4
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(308, 192)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(85, 34)
+        Me.btnExit.TabIndex = 5
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(449, 238)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnCheck)
+        Me.Controls.Add(Me.lblMessage)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Loan Qualifier"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents txtYearsAtJob As System.Windows.Forms.TextBox
+    Friend WithEvents txtAnnualSalary As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents lblMessage As System.Windows.Forms.Label
+    Friend WithEvents btnCheck As System.Windows.Forms.Button
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+
+End Class
---
+++ LoanQualifier/LoanQualifier/Form1.vb
@@ -0,0 +1,40 @@
+﻿Public Class Form1
+
+    Private Sub btnCheck_Click(sender As Object, e As EventArgs) Handles btnCheck.Click
+        Dim dblSalary As Double
+        Dim intYearsOnJob As Integer
+
+        Try
+            dblSalary = CDbl(txtAnnualSalary.Text)
+            intYearsOnJob = CInt(txtYearsAtJob.Text)
+
+            If dblSalary > 30000 Then
+                If intYearsOnJob > 2 Then
+                    lblMessage.Text = "The applicant qualifies"
+                Else
+                    lblMessage.Text = "The applicant does notqualifies"
+                End If
+            Else
+                If intYearsOnJob > 5 Then
+                    lblMessage.Text = "The applicant qualifies"
+                Else
+                    lblMessage.Text = "The applicant does not qualifies"
+                End If
+            End If
+        Catch
+            lblMessage.Text = "Please enter numeric values."
+        End Try
+
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        lblMessage.Text = String.Empty
+        txtAnnualSalary.Clear()
+        txtYearsAtJob.Clear()
+        txtAnnualSalary.Focus()
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+End Class
