806fb236a9e5703f7cbee7eedb11e1dc99a04a1b
Andrew <heldiggris7@gmail.com>
2017-11-21 11:31:37 -0600

    update RunningtheRace

---
+++ RunningtheRace/RunningtheRace/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("RunningtheRace")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Davenport Community School District")> 
+<Assembly: AssemblyProduct("RunningtheRace")> 
+<Assembly: AssemblyCopyright("Copyright © Davenport Community School District 2017")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("b0f0f59b-99f5-48e0-ad61-5b21042d12df")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
